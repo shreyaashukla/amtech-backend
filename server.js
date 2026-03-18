b0a8f01513d8f4ee5046914f1cb7034fa53a5c05
Shreya Shukla <your-email@example.com>
2026-03-19 01:37:35 +0530

    Fix CORS

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const app = express();
 /* ======================
    Middleware
 ====================== */
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 /* ======================
